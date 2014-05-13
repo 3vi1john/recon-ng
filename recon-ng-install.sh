8bbf0cb7cb008be4cef15e694a0ab995f956ae9b
3vi1john <jbabio@me.com>
2014-05-12 21:13:17 -0400

    prereqs

---
+++ recon-ng-install.sh
@@ -0,0 +1,7 @@
+wget http://www.dnspython.org/kits/1.11.1/dnspython-1.11.1.zip
+svn checkout http://slowaes.googlecode.com/svn/trunk/ slowaes-read-only
+git clone https://github.com/joshmarshall/jsonrpclib.git
+wget http://pypi.python.org/packages/source/m/mechanize/mechanize-0.2.5.tar.gz
+git clone https://github.com/quandyfactory/dicttoxml.git
+
+git clone https://LaNMaSteR53@bitbucket.org/LaNMaSteR53/recon-ng.git
